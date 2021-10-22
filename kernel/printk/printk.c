cebf22c30b784645a9d65c1522b524de88e31120
fajar4561 <maulanafajar751@gmail.com>
2021-10-22 13:36:49 +0700

    SQUASH: Remove some log spam
    Signed-off-by: fajar4561 <maulanafajar751@gmail.com>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -658,6 +658,11 @@ static ssize_t devkmsg_write(struct kiocb *iocb, struct iov_iter *from)
 			endp++;
 			len -= endp - line;
 			line = endp;
+			 if (strstr(line,"init") || strstr(line,"healthd") || strstr(line,"logd"))
+			{
+			   kfree(buf);
+			   return ret;
+			}
 		}
 	}
 
