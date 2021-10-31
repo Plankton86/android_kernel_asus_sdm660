e8c347307c95100269b5299f36837441767430c0
psndna88 <psndna88@gmail.com>
2021-10-31 04:05:48 +0000

    qpnp-fg-gen3: force adherance to max charging current limitations
    Change-Id: I4cc57345243299623c69eceb52079c756d68e141
    Signed-off-by: ben863 <r.budhiono@gmail.com>

--- drivers/power/supply/qcom/qpnp-fg-gen3.c
+++ drivers/power/supply/qcom/qpnp-fg-gen3.c
@@ -3289,6 +3289,11 @@ static int fg_get_time_to_full_locked(struct fg_chip *chip, int *val)
 	ibatt_avg = -ibatt_avg / MILLI_UNIT;
 	vbatt_avg /= MILLI_UNIT;
 
+    #ifdef CONFIG_MACH_ASUS_X00TD
+    if (ibatt_avg > 2500)
+		ibatt_avg = 2500; /* force max charging current limitations */
+    #endif
+
 	/* clamp ibatt_avg to iterm */
 	if (msoc <= 90) {
 		if (ibatt_avg < 3000)
